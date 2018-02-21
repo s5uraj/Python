01de6b71379661f2b87f0e82725668cb113f0842
Surajkumar <35773226+s5uraj@users.noreply.github.com>
2018-02-21 21:58:03 +0530

    Update mini_prj.py

--- mini_prj.py
+++ mini_prj.py
@@ -1,16 +1,32 @@
 #A program asks user to enter file name and scrambles each word of it having length more than 3 except punctation symbols.
 import random
+new_list=[]
+def rec(my_list):
+    if not my_list:
+        return new_list
+    else:
+        ran=random.randint(0,len(my_list)-1)
+        new_list.append(my_list.pop(ran))
+        rec(my_list)
+    return new_list
+
 file=input("Enter File name")
 fd=open(file,'r')
-word=fd.read()
-fd=open(file,'r')
-my_list = [i for i in word]
-def idword(word):
-    count=0
-    for line in word:
-      print("0")  
+var=fd.read()
+#var="suraj nad. howww . aree yu"
+for x in var.split():
+    if len(x) > 3:
+        if x.find(".") ==-1:
+            x = x.replace(".", "")
+            my_list = [i for i in x]
+            sc = ''.join(rec(my_list))
+            print(sc)
+            
+    
 
-idword(fd)
-print(my_list)
+
+
+
+
 
 
